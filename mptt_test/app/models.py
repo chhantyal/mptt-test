16c732103ecec1f9ee79e1ddaa66471760ab920a
Nar Chhantyal <neokya@gmail.com>
2015-01-19 22:09:16 +0100

    add initial setup

---
+++ mptt_test/app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from mptt.models import MPTTModel, TreeForeignKey
+from mptt.managers import TreeManager
+
+
+class ResourceAbstract(MPTTModel):
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.SET_NULL)
+    content_type = models.SlugField(editable=False)
+
+    title = models.CharField(max_length=100)
+
+    objects = tree = TreeManager()
+
+    class Meta:
+    	abstract = True
+
+
+class Page(ResourceAbstract):
+	body = models.TextField(blank=True)
